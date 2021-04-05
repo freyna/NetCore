2b8991949243212272a4b9918c9b31fa605950c1
Fily <ing.filiberto.reyna@gmail.com>
2021-04-05 11:21:14 -0500

    Entidad Curso
    Se agrega solución y funcionalidad con las entidad curso.

---
+++ LeerData/AppVentaCursosContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+
+namespace LeerData
+{
+    public class AppVentaCursosContext : DbContext
+    {
+        private const string connectionString = @"Data Source=192.168.0.31;Initial Catalog=CursosOnline;User Id=sa;Password=Kwn20191!";
+
+        public DbSet<Curso> Curso {get;set;}
+        public DbSet<Precio> Precio {get;set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ LeerData/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace LeerData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola Alumnos!");
+
+            using (var db = new AppVentaCursosContext())
+            {
+                var cursos = db.Curso.AsNoTracking();
+                
+                foreach (var curso in cursos)
+                {
+                    Console.WriteLine(curso.Titulo);
+                }
+            }
+        }
+    }
+}
