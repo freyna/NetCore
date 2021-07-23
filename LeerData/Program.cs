31b39ea8805068dcbe371fb4fb6064be6056a397
Fily <ing.filiberto.reyna@gmail.com>
2021-07-23 08:54:28 -0500

    Se agregan las operaciones insert, update y delete.

--- LeerData/Program.cs
+++ LeerData/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
+using System.Linq;
 
 namespace LeerData
 {
@@ -8,7 +9,7 @@ namespace LeerData
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("¡Hola Alumnos!");
+            //Console.WriteLine("¡Hola Alumnos!");
 
             using (var db = new AppVentaCursosContext())
             {
@@ -21,16 +22,74 @@ namespace LeerData
                 // }
                 #endregion
 
-                var cursos = db.Curso.Include(item=>item.InstructorLink).ThenInclude(item=>item.Instructor);
+                #region Segundo ejemplo
 
-                foreach(var curso in cursos)
+                // var cursos = db.Curso.Include(item=>item.InstructorLink).ThenInclude(item=>item.Instructor);
+
+                // foreach(var curso in cursos)
+                // {
+                //     Console.WriteLine(curso.Titulo);
+                //     foreach (var instructorLink in curso.InstructorLink)
+                //     {
+                //         Console.WriteLine("====> " + instructorLink.Instructor.Nombre);
+                //     }
+                // }
+                #endregion
+            
+                #region Tercer ejemplo Insert
+                // var nuevoInstructor = new Instructor
+                // {
+                //     Nombre = "Juan",
+                //     Apellidos = "Pérez",
+                //     Grado = "Master en computación"
+                // };
+
+                // db.Add(nuevoInstructor);
+
+                // var nuevoInstructor2 = new Instructor
+                // {
+                //     Nombre = "José",
+                //     Apellidos = "Mariano",
+                //     Grado = "Master en computación"
+                // };
+
+                // db.Add(nuevoInstructor2);
+
+                // var estadoTransaccion = db.SaveChanges();
+
+                // Console.WriteLine("Estado de la transacción ==>" + estadoTransaccion);
+
+                #endregion
+            
+                #region Cuarto ejemplo update
+
+                // var instructor = db.Instructor.Single(p=>p.Nombre == "Lorenzo");
+
+                // if(instructor != null)
+                // {
+                //     instructor.Apellidos = "Castro de Romana";
+                //     instructor.Grado = "Biologo con experiencia";
+
+                //     var estadoTransaccion = db.SaveChanges();
+
+                //     Console.WriteLine("El estado de la transacción ==>"+ estadoTransaccion);
+                // }
+                #endregion
+            
+                #region Quinto ejemplo delete
+
+                var instructor = db.Instructor.Single(p=>p.InstructorId == 7);
+
+                if(instructor != null)
                 {
-                    Console.WriteLine(curso.Titulo);
-                    foreach (var instructorLink in curso.InstructorLink)
-                    {
-                        Console.WriteLine("====> " + instructorLink.Instructor.Nombre);
-                    }
+                    db.Remove(instructor);
+
+                    var estadoTransaccion = db.SaveChanges();
+
+                    Console.WriteLine("Estado de la transacción ==> " + estadoTransaccion);
                 }
+
+                #endregion
             }
         }
     }
